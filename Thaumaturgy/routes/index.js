1d05e0d80be05d0df4558200a9975510ca9c7b94
xTumulus <logankitch@gmail.com>
2017-11-02 20:36:47 -0600

    implemented express framework

--- Thaumaturgy/routes/index.js
+++ Thaumaturgy/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html', { root: 'views' });
 });
+//
+// router.get('/game', function(req, res, next) {
+//   res.render('game', { root: 'views' });
+// });
+//
+// router.get('/results', function(req, res, next) {
+//   res.render('', { root: 'views' });
+// });
+//
+// router.get('/scoreboard', function(req, res, next) {
+//   res.render('index', { root: 'views' });
+// });
 
 module.exports = router;
